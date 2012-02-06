98b3d6a04013d1d10fd27724491ba28d9725cf89
Ronald Hecker <selsery@gmail.com>
2012-02-06 19:03:10 +0100

    Adds custom model for cube with texture coordinates

---
+++ lib/models/cube.rb
@@ -0,0 +1,39 @@
+def drawCube size
+  #front
+  glTexCoord( 0.0, 1.0); glVertex(-size,-size, size)
+  glTexCoord( 1.0, 1.0); glVertex(-size, size, size)
+  glTexCoord( 1.0, 0.0); glVertex( size, size, size)
+  glTexCoord( 0.0, 0.0); glVertex( size,-size, size)
+
+  #back
+  glTexCoord( 0.0, 1.0); glVertex(-size,-size,-size)
+  glTexCoord( 1.0, 1.0); glVertex(-size, size,-size)
+  glTexCoord( 1.0, 0.0); glVertex( size, size,-size)
+  glTexCoord( 0.0, 0.0); glVertex( size,-size,-size)
+
+  #top
+  glTexCoord( 0.0, 0.0); glVertex(-size, size,-size)
+  glTexCoord( 0.0, 1.0); glVertex(-size, size, size)
+  glTexCoord( 1.0, 1.0); glVertex( size, size, size)
+  glTexCoord( 1.0, 0.0); glVertex( size, size,-size)
+
+  #bottom
+  glTexCoord( 0.0, 0.0); glVertex(-size,-size,-size)
+  glTexCoord( 0.0, 1.0); glVertex(-size,-size, size)
+  glTexCoord( 1.0, 1.0); glVertex( size,-size, size)
+  glTexCoord( 1.0, 0.0); glVertex( size,-size,-size)
+
+  #left
+  glTexCoord( 0.0, 0.0); glVertex(-size,-size,-size)
+  glTexCoord( 0.0, 1.0); glVertex(-size,-size, size)
+  glTexCoord( 1.0, 1.0); glVertex(-size, size, size)
+  glTexCoord( 1.0, 0.0); glVertex(-size, size,-size)
+
+  #right
+  glTexCoord( 0.0, 0.0); glVertex( size,-size,-size)
+  glTexCoord( 0.0, 1.0); glVertex( size,-size, size)
+  glTexCoord( 1.0, 1.0); glVertex( size, size, size)
+  glTexCoord( 1.0, 0.0); glVertex( size, size,-size)
+
+  glEnd
+end
