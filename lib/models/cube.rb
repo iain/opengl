6c4b04a09f54382da920266678119ea3c9ff9ec1
Ronald Hecker <selsery@gmail.com>
2012-02-06 19:05:20 +0100

    Adds explanation for model

--- lib/models/cube.rb
+++ lib/models/cube.rb
@@ -1,4 +1,6 @@
 def drawCube size
+  #glBegin(GL_QUADS) #this is required before the call thus making the choice for wired or solid
+  #possible
   #front
   glTexCoord( 0.0, 1.0); glVertex(-size,-size, size)
   glTexCoord( 1.0, 1.0); glVertex(-size, size, size)
