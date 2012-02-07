6c28d025ebc31bb2886eea8eea4c99226ffab00a
Ronald Hecker <selsery@gmail.com>
2012-02-08 00:09:40 +0100

    Change sphere to cube

--- lib/views/grid_view.rb
+++ lib/views/grid_view.rb
@@ -4,6 +4,7 @@ class GridView < View
     super
 
     @buffers = nil
+    @field_size = 10.0
   end
 
   def buffered?
@@ -23,7 +24,39 @@ class GridView < View
   end
 
   def drawSky
-    glutSolidSphere(1280/2,36,36)
+    width = @model.width * @field_size
+    depth = @model.height * @field_size
+    height = width
+
+    vertices = [
+       -width, height, +depth,
+       -width, height, -depth,
+       +width, height, -depth,
+       +width, height, +depth,
+       -width, -20, +depth,
+       +width, -20, +depth,
+       +width, -20, -depth,
+       -width, -20, -depth
+    ]
+
+    indices = [
+        0, 1, 2, 3,
+        4, 7, 6, 5,
+        0, 4, 7, 1,
+        3, 2, 6, 5,
+        7, 6, 2, 1,
+        4, 5, 3, 0
+      ]
+
+    glEnableClientState(GL_VERTEX_ARRAY)
+
+    glVertexPointer(3, GL_FLOAT, 0, vertices)
+
+    glDrawElements(GL_QUADS, 24, GL_UNSIGNED_BYTE, indices)
+
+    glBindBuffer(GL_ARRAY_BUFFER, 0)
+
+    glDisableClientState(GL_VERTEX_ARRAY)
   end
 
   def set_buffers
@@ -51,17 +84,16 @@ class GridView < View
 
     glBindBuffer(GL_ARRAY_BUFFER_ARB, @buffers[0])
 
-    field_size = 10.0
 
     vertices = [
-       -field_size, 3, +field_size,
-       -field_size, 3, -field_size,
-       +field_size, 3, -field_size,
-       +field_size, 3, +field_size,
-       -field_size, 0, +field_size,
-       +field_size, 0, +field_size,
-       +field_size, 0, -field_size,
-       -field_size, 0, -field_size
+       -@field_size, 3, +@field_size,
+       -@field_size, 3, -@field_size,
+       +@field_size, 3, -@field_size,
+       +@field_size, 3, +@field_size,
+       -@field_size, 0, +@field_size,
+       +@field_size, 0, +@field_size,
+       +@field_size, 0, -@field_size,
+       -@field_size, 0, -@field_size
     ].pack("f*")
 
     glBufferData(GL_ARRAY_BUFFER, 6*8*3, vertices,GL_DYNAMIC_DRAW)
@@ -87,11 +119,10 @@ class GridView < View
         7, 6, 2, 1,
         4, 5, 3, 0
       ]
-    field_size = 10.0
 
-    x *= 2*field_size
+    x *= 2*@field_size
     y -= 20
-    z *= 2*field_size
+    z *= 2*@field_size
 
     glTranslate(x,y,z)
 
