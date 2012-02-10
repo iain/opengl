f218917a58a9c01ec7a27a6448c05bf027e3077a
iain <iain@iain.nl>
2012-02-11 00:00:13 +0100

    Translate and rotate

--- spec/mechanics/rotation_spec.rb
+++ spec/mechanics/rotation_spec.rb
@@ -5,101 +5,120 @@ class RotatableObject
   include Rotation
 end
 
-describe RotatableObject, "a class that includes Rotation" do
+describe Rotation do
 
-  it "exposes the rotation" do
-    subject.rotation.should be_a(Rotation::Rotator)
-  end
+  describe "with a class that includes Rotation" do
 
-  it "exposes the matrix (needed so much for OpenGL, it is public)" do
-    subject.matrix.object_id.should == subject.rotation.matrix.object_id
-  end
+    subject { RotatableObject.new }
 
-end
+    it "exposes the rotation" do
+      subject.rotation.should be_a(Rotation::Rotator)
+    end
 
-
-describe Rotation::Rotator, "the actual object that does the rotation" do
-
-  context "at the start" do
-
-    it "starts unrotated and centered (a plain identity matrix)" do
-      subject.matrix.should == Matrix.identity(4)
+    it "exposes the matrix (needed so much for OpenGL, it is public)" do
+      subject.matrix.object_id.should == subject.rotation.matrix.object_id
     end
 
   end
 
-  describe "#yaw!" do
 
-    it "rotates along the x axis" do
-      subject.yaw! Math::PI / 2
-      subject.matrix.should be_matrix_like(
-        [ 0, -1, 0, 0 ],
-        [ 1,  0, 0, 0 ],
-        [ 0,  0, 1, 0 ],
-        [ 0,  0, 0, 1 ]
-      )
+  describe Rotation::Rotator do
+
+    subject { Rotation::Rotator.new }
+
+    context "at the start" do
+
+      it "starts unrotated and centered (a plain identity matrix)" do
+        subject.matrix.should == Matrix.identity(4)
+      end
+
     end
 
-  end
+    describe "#yaw!" do
 
-  describe "#pitch!" do
+      it "rotates along the x axis" do
+        subject.yaw! Math::PI / 2
+        subject.matrix.should be_matrix_like(
+          [ 0, -1, 0, 0 ],
+          [ 1,  0, 0, 0 ],
+          [ 0,  0, 1, 0 ],
+          [ 0,  0, 0, 1 ]
+        )
+      end
 
-    it "rotates along the y axis" do
-      subject.pitch! Math::PI / 2
-      subject.matrix.should be_matrix_like(
-        [ 1, 0,  0, 0 ],
-        [ 0, 0, -1, 0 ],
-        [ 0, 1,  0, 0 ],
-        [ 0, 0,  0, 1 ]
-      )
     end
 
-  end
+    describe "#pitch!" do
 
-  describe "#roll!" do
+      it "rotates along the y axis" do
+        subject.pitch! Math::PI / 2
+        subject.matrix.should be_matrix_like(
+          [ 1, 0,  0, 0 ],
+          [ 0, 0, -1, 0 ],
+          [ 0, 1,  0, 0 ],
+          [ 0, 0,  0, 1 ]
+        )
+      end
 
-    it "rotates along the z axis" do
-      subject.roll! Math::PI / 2
-      subject.matrix.should be_matrix_like(
-        [  0, 0, 1, 0 ],
-        [  0, 1, 0, 0 ],
-        [ -1, 0, 0, 0 ],
-        [  0, 0, 0, 1 ]
-      )
     end
 
-  end
+    describe "#roll!" do
 
-  describe "#translate!" do
+      it "rotates along the z axis" do
+        subject.roll! Math::PI / 2
+        subject.matrix.should be_matrix_like(
+          [  0, 0, 1, 0 ],
+          [  0, 1, 0, 0 ],
+          [ -1, 0, 0, 0 ],
+          [  0, 0, 0, 1 ]
+        )
+      end
 
-    it "moves along x axis" do
-      subject.translate!(10, 0, 0)
-      subject.matrix.should be_matrix_like(
-        [  1, 0, 0, 0 ],
-        [  0, 1, 0, 0 ],
-        [  0, 0, 1, 0 ],
-        [ 10, 0, 0, 1 ]
-      )
     end
 
-    it "moves along y axis" do
-      subject.translate!(0, 2, 0)
-      subject.matrix.should be_matrix_like(
-        [  1, 0, 0, 0 ],
-        [  0, 1, 0, 0 ],
-        [  0, 0, 1, 0 ],
-        [  0, 2, 0, 1 ]
-      )
-    end
+    describe "#translate!" do
 
-    it "moves along z axis" do
-      subject.translate!(0, 0, 4)
-      subject.matrix.should be_matrix_like(
-        [  1, 0, 0, 0 ],
-        [  0, 1, 0, 0 ],
-        [  0, 0, 1, 0 ],
-        [  0, 0, 4, 1 ]
-      )
+      it "moves along x axis" do
+        subject.translate!(10, 0, 0)
+        subject.matrix.should be_matrix_like(
+          [  1, 0, 0, 0 ],
+          [  0, 1, 0, 0 ],
+          [  0, 0, 1, 0 ],
+          [ 10, 0, 0, 1 ]
+        )
+      end
+
+      it "moves along y axis" do
+        subject.translate!(0, 2, 0)
+        subject.matrix.should be_matrix_like(
+          [  1, 0, 0, 0 ],
+          [  0, 1, 0, 0 ],
+          [  0, 0, 1, 0 ],
+          [  0, 2, 0, 1 ]
+        )
+      end
+
+      it "moves along z axis" do
+        subject.translate!(0, 0, 4)
+        subject.matrix.should be_matrix_like(
+          [  1, 0, 0, 0 ],
+          [  0, 1, 0, 0 ],
+          [  0, 0, 1, 0 ],
+          [  0, 0, 4, 1 ]
+        )
+      end
+
+      it "moves the object, keeping its rotation in mind" do
+        subject.roll! Math::PI / 2
+        subject.translate! 1, 0, 0
+        subject.matrix.should be_matrix_like(
+          [  0, 0, 1, 0 ],
+          [  0, 1, 0, 0 ],
+          [ -1, 0, 0, 0 ],
+          [  0, 0, 1, 1 ]
+        )
+      end
+
     end
 
   end
--- spec/support/matrix_matcher.rb
+++ spec/support/matrix_matcher.rb
@@ -5,8 +5,12 @@ RSpec::Matchers.define :be_matrix_like do |*expected|
     @diffs = []
     actual.each_with_index do |value, row, col|
       e = expected_matrix[row, col]
-      if (value - e).abs > delta
+      if e.nil?
         @diffs << [ row, col ]
+      else
+        if (value - e).abs > delta
+          @diffs << [ row, col ]
+        end
       end
     end
     @diffs.empty?
@@ -29,13 +33,19 @@ RSpec::Matchers.define :be_matrix_like do |*expected|
   def pretty_matrix(matrix)
     matrix.to_a.map.with_index { |row, row_index|
       "[ #{format_row(row, row_index)} ] #{row_indicator(row_index)}"
-    }.join("\n")
+    }.join("\n") + "\n" + column_indicator(matrix)
   end
 
   def format_row(row, row_index)
-    row.map.with_index { |val, col_index|
-      val >= 0 ? (" %.3f" % val) : ("%.3f" % val)
-    }.join(", ")
+    row.map.with_index { |val, col_index| format_value(val) }.join(", ")
+  end
+
+  def format_value(val)
+    if val.is_a?(Numeric)
+      (val >= 0 ? (" %.3f" % val) : ("%.3f" % val)).rjust(7)
+    else
+      val.inspect.rjust(7)
+    end
   end
 
   def row_indicator(row_index)
@@ -44,6 +54,16 @@ RSpec::Matchers.define :be_matrix_like do |*expected|
     else
       ""
     end
+  end
+
+  def column_indicator(matrix)
+    "  " + matrix.row(0).to_a.map.with_index { |val, index|
+      if @diffs.find { |(row, col)| col == index }
+        "^   ".rjust(7)
+      else
+        " " *  7
+      end
+    }.join('  ')
   end
 
 end
