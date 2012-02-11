ae252b51dc17d433711ebb04666224e50c8fb21d
iain <iain@iain.nl>
2012-02-11 22:20:21 +0100

    Camera specs

---
+++ spec/walker/camera_spec.rb
@@ -0,0 +1,53 @@
+require 'walker/rotation'
+require 'walker/camera'
+require 'support/matrix_matcher'
+
+describe Walker::Camera do
+
+  it "has a default position and rotation" do
+    subject.matrix.should == Matrix.identity(4)
+  end
+
+  it "can zoom in" do
+    subject.zoom_in
+    subject.matrix.should be_matrix_like(
+      [ 1, 0,   0, 0 ],
+      [ 0, 1,   0, 0 ],
+      [ 0, 0,   1, 0 ],
+      [ 0, 0, 0.1, 1 ]
+    )
+  end
+
+  it "can zoom out" do
+    subject.zoom_out
+    subject.matrix.should be_matrix_like(
+      [ 1, 0,    0, 0 ],
+      [ 0, 1,    0, 0 ],
+      [ 0, 0,    1, 0 ],
+      [ 0, 0, -0.1, 1 ]
+    )
+  end
+
+  it "can zoom out, after rotating" do
+    subject.zoom_out
+    subject.roll Math::PI / 2
+    subject.matrix.should be_matrix_like(
+      [  0, 0,    1, 0 ],
+      [  0, 1,    0, 0 ],
+      [ -1, 0,    0, 0 ],
+      [  0, 0, -0.1, 1 ]
+    )
+  end
+
+  it "can zoom in, after rotating" do
+    subject.zoom_in
+    subject.roll Math::PI / 2
+    subject.matrix.should be_matrix_like(
+      [  0, 0,   1, 0 ],
+      [  0, 1,   0, 0 ],
+      [ -1, 0,   0, 0 ],
+      [  0, 0, 0.1, 1 ]
+    )
+  end
+
+end
