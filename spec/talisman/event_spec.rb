d4ed8c42a3b3a5da5186d978b2f84d2a51f85bbf
iain <iain@iain.nl>
2012-02-15 21:08:03 +0100

    Missing specs for what the other class drove out

--- spec/talisman/event_spec.rb
+++ spec/talisman/event_spec.rb
@@ -1,6 +1,21 @@
 require 'talisman/event'
 
 describe Talisman::Event do
-  it "Works" do
+
+  it "sets the x attribute" do
+    expect { subject.trigger x: 1 }.to change { subject.x }.to 1
   end
+
+  it "sets the y attribute" do
+    expect { subject.trigger y: 2 }.to change { subject.y }.to 2
+  end
+
+  it "sets the key attribute" do
+    expect { subject.trigger key: "e" }.to change { subject.key }.to "e"
+  end
+
+  it "sets the type attribute" do
+    expect { subject.trigger type: :mouse }.to change { subject.type }.to :mouse
+  end
+
 end
