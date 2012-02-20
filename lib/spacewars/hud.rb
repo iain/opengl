db112d81085651ef7beb009473ab33d1fa675e12
Ronald Hecker <selsery@gmail.com>
2012-02-20 19:01:10 +0100

    first attempt at hud

--- lib/spacewars/hud.rb
+++ lib/spacewars/hud.rb
@@ -4,18 +4,28 @@ end
 
 class HudView < Walker::View
   def draw!
-    glDepthFunc(GL_LESS);
-
+    width, height = 700, 450
+    glDisable(GL_DEPTH_TEST)
+    glMatrixMode(GL_PROJECTION)
+    glLoadIdentity()
+    gluOrtho2D(0, width, 0, height)
+    glMatrixMode(GL_MODELVIEW)
     glLoadIdentity
+    # glDepthFunc(GL_LESS);
+
     glColor(0.5,0.5,0.5)
     glBegin(GL_QUADS)
-    glVertex(0, 1)
-    glVertex(1, 1)
-    glVertex(1, 0)
-    glVertex(0, 0)
+    glVertex(0  , 100)
+    glVertex(100, 100)
+    glVertex(100, 0)
+    glVertex(0  , 0)
     glEnd
 
-    glDepthMask(GL_TRUE);
-
+    # glDepthMask(GL_TRUE);
+    glMatrixMode(GL_PROJECTION)
+    glLoadIdentity
+    gluPerspective(18, width / height, 1e5, 1e18)
+    glMatrixMode(GL_MODELVIEW)
+    glLoadIdentity
   end
 end
